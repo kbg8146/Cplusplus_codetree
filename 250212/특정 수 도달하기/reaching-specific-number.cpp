87cd808798f5ee2fb8b006b677495e455a5f5f61
kbg8146 <109897476+kbg8146@users.noreply.github.com>
2025-02-12 18:46:30 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250212/특정 수 도달하기/reaching-specific-number.cpp
+++ 250212/특정 수 도달하기/reaching-specific-number.cpp
@@ -6,19 +6,20 @@ int main() {
     int arr[9];
     int n=0;
     int sum=0;
-
+    double avg=0;
     for(int i=0;i<10;i++){
         cin >> arr[i];
         if(i<9){
             if(arr[i]>=250){    
-                double avg=(double)sum/i;
+                avg=(double)sum/i;
                 cout << sum << " " << avg;
                 break;
             }
         }
         sum+=arr[i];
         if(i==9){
-            cout << sum << " " << (double)sum/10;
+            avg=(double)sum/(i+1);
+            cout << sum << " " << avg;
             break;
         }
         

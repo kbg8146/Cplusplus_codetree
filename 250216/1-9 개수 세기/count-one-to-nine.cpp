01b6babf3198b4489a9319968508a442656e8df4
kbg8146 <109897476+kbg8146@users.noreply.github.com>
2025-02-16 16:28:44 +0900

    Title: 1-9 개수 세기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250216/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int arr[11]={};
+    int count_arr[11]={};
+
+    for(int i=1;i<=n;i++){
+        cin >> arr[i];
+    }
+    for(int i=1;i<=n;i++){
+        count_arr[arr[i]]++;
+    }
+    for(int i=1;i<10;i++){
+        cout << count_arr[i] << endl;
+    }
+    return 0;
+}

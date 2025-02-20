ce3771e0017c83247dfbbaa578b3a83c1a984f26
kbg8146 <109897476+kbg8146@users.noreply.github.com>
2025-02-20 22:55:12 +0900

    Title: 정수 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250220/정수 직사각형/rectangle-of-numbers.cpp
+++ 250220/정수 직사각형/rectangle-of-numbers.cpp
@@ -8,8 +8,7 @@ int main() {
     int arr[100][100]={};
     int k=1;
     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            
+        for(int j=0;j<m;j++){ 
             arr[i][j]=k;
             k++;
         }
@@ -18,7 +17,7 @@ int main() {
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cout << arr[i][j] << " ";
-            if(arr[i][j]%3==0){
+            if(arr[i][j]%m==0){
                 cout << endl;
             }
         }

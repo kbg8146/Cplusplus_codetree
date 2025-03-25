680ecf0ea3a851d490070b397369885a37a675ba
kbg8146 <109897476+kbg8146@users.noreply.github.com>
2025-03-25 19:02:42 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250325/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
+++ 250325/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 bool IsMagicNumber(int n){
     int second, first = 0;
+    int result=0;
     second = n/10;
-    first = n-second;
-    return second+first;
+    first = n-second*10;
+    result = second+first;
+
+    return n%2==0 && result%5==0;
 }
 
 int main() {

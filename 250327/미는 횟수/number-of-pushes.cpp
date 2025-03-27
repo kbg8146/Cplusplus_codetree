9f21d38f64705e3feae92c78deb138fb072ac61a
kbg8146 <109897476+kbg8146@users.noreply.github.com>
2025-03-27 16:17:33 +0900

    Title: 미는 횟수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250327/미는 횟수/number-of-pushes.cpp
+++ 250327/미는 횟수/number-of-pushes.cpp
@@ -8,7 +8,11 @@ int main() {
     cin >> A >> B;
     int len = A.length();
     int cnt=0;
-
+    int small=0;
+    if(A==B){
+        cout << 1;
+        return 0;
+    }
     for(int i=0;i<len;i++){
         if(A[i]==B[i]){
             continue;

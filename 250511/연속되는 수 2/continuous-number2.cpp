b0593383739a4f27277cc1f47e5ba5bbd55afa9f
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-05-11 00:42:49 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250511/연속되는 수 2/continuous-number2.cpp
+++ 250511/연속되는 수 2/continuous-number2.cpp
@@ -7,13 +7,25 @@ int main() {
     int n;
     int cnt=0;
     int a[1010]={0};
+    int big=0;
     for(int i=0;i<N;i++){
         cin >> a[i];
 
-        if(i==0||a[i] != a[i-1])
+        if(i==0||a[i] != a[i-1]){
+            if(cnt>big){
+                cnt++;
+                big=cnt;
+            }
+            cnt=0;
+        }
+        else{
             cnt++;
+            
+        }
     }
-    cout << cnt;
+
+
+    cout << big;
 
     // Please write your code here.
     return 0;

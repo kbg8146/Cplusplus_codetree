fd696b36675770cffa37112960b5684934dec0bc
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-05-11 16:01:01 +0900

    Title: 만나는 그 순간, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250511/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N,M;
+    cin >> N >> M;
+    int SIZE = 2000;
+    int a[2000]={0};
+    int b[2000]={0};
+
+    string dir;
+    int step=0;
+    int timeA=0;
+    int timeB=0;
+    int stateA=1000;
+    int stateB=1000;
+    int result=-1;
+    for(int i=0;i<N;i++){
+        cin >> dir >> step;
+        if(dir=="R"){
+            for(int k=1;k<=step;k++){
+                a[stateA++]=timeA++;
+            }
+        }
+        else if(dir=="L"){
+            for(int k=1;k<=step;k++){
+                a[stateA--]=timeA++;
+            }
+        }
+        
+    }
+    for(int j=0;j<M;j++){
+        cin >> dir>> step;
+
+        if(dir=="R"){
+            for(int k=1;k<=step;k++){
+                b[stateB++]=timeB++;
+            }
+        }
+        else if(dir=="L"){
+            for(int k=1;k<=step;k++){
+                b[stateB--]=timeB++;
+            }
+        }
+    }
+
+    for(int i=0;i<2000;i++){
+        if(a[i]==b[i]&&a[i]!=0&&b[i]!=0){
+            result = a[i];
+            break;
+        }
+    }
+    cout << result;
+    // Please write your code here.
+    return 0;
+}

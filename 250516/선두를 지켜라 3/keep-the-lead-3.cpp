fb9da8da9ff6c8e634724f5574900bcb7da62e28
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-05-16 16:36:09 +0900

    Title: 선두를 지켜라 3, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250516/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    //cout << N << " " << M << endl;
+    int A[1004]={0};
+    int B[1004]={0};
+    int Av=0;
+    int At=0;
+    int Bv=0;
+    int Bt=0;
+    int time=1;
+    for(int i=0;i<N;i++){
+        cin >> Av >> At;
+        for(int j=1;j<=At;j++){
+            A[time++]=Av;
+           // cout << Av << " ";
+        }
+    }
+    time=1;
+    //cout << endl;
+    for(int i=0;i<M;i++){
+        cin >> Bv >> Bt;
+        for(int j=1;j<=Bt;j++){
+            B[time++]=Bv;
+            //cout << Bv << " ";
+        }
+    }
+    //cout << endl;
+    int cnt=0;
+    int sumA=0;
+    int sumB=0;
+    int compare[1004]={0};
+    for(int i=1;i<1004;i++){
+        sumA+=A[i];
+        sumB+=B[i];
+
+        if(sumA>sumB){
+            compare[i]=1;
+        }
+        else if(sumA<sumB){
+            compare[i]=2;
+        }
+        else{
+            compare[i]=0;
+        }
+        //cout << sumA << " + " << A[i] << endl;
+        
+        if(compare[i]!=compare[i-1] /*&& compare[i]!=0*/){
+            cnt++;
+        }
+    }
+    cout << cnt;
+    // Please write your code here.
+    return 0;
+}

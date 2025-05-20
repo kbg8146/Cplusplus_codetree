07a73deb5b9915c8df4dfda4a1b229e7ff92bf02
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-05-20 19:20:47 +0900

    Title: 문자에 따른 명령 2, Time: 1030ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 250520/문자에 따른 명령 2/text-based-commands2.cpp
+++ 250520/문자에 따른 명령 2/text-based-commands2.cpp
@@ -4,22 +4,27 @@ using namespace std;
 int main() {
     char dir;
     char move;
-    cin >> dir >> move;
+
 
     int dx[4]={0,1,0,-1};
     int dy[4]={1,0,-1,0};
     int dir_num=50000;
     int x=0,y=0;
+    while(1){
+        cin >> dir;
+        if(dir=='R'){
+            dir_num=(dir_num+1)%4;
+        }else if(dir=='L'){
+            dir_num=(dir_num-1)%4;
+        }
 
-    if(dir=='R'){
-        dir_num=(dir_num+1)%4;
-    }else if(dir=='L'){
-        dir_num=(dir_num-1)%4;
+        if(dir=='F'){       
+            x = x + dx[dir_num];
+            y = y + dy[dir_num];
+            break;
+        }
+
     }
-
-    x= x + dx[dir_num];
-    y= y + dy[dir_num];
-
     cout << x << " " << y;
 
     return 0;

84b79150dbfb5f354885fb60d21cc54283f554f7
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-05-20 20:11:10 +0900

    Title: 1이 3개 이상 있는 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250520/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 250520/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -21,12 +21,11 @@ int main() {
     }
 
     int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
-    int cnt=0;
     int x=0,y=0;
     int ans=0;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-
+            int cnt=0;
             for(int dir_num=0;dir_num<4;dir_num++){
                 int x=i,y=j;
                 x = x + dx[dir_num];
@@ -37,7 +36,7 @@ int main() {
                 }
             }                
             if(cnt>=3){
-                    cnt=0;
+
                     ans++;
             }      
         }

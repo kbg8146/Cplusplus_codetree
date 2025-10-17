80da2dfa31d0b7808c62d6130098762a3157afc1
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-10-18 01:40:43 +0900

    Title: 최대로 겹치는 구간, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 251018/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 251018/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -6,17 +6,20 @@ int main() {
     cin >> n;
 
     int a, b;
+    int bmax=0;
     int arr[1000]={0};
     for(int i=0;i<n;i++){
         cin >> a >> b;
-
+        if(b>=bmax){
+            bmax=b;
+        }
         for(int j=a;j<b;j++){
             ++arr[j];
         }
     }
 
     int max=0;
-    for(int i=0;i<100;i++){
+    for(int i=0;i<bmax;i++){
         if(arr[i]>=max){
             max=arr[i];
         }

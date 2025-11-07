e6b2e22d7b4e959a87e17d397409496376bf2edd
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-11-07 19:40:40 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251107/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
+++ 251107/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -10,9 +10,9 @@ int main() {
     cin >> arr;
 
     int cnt=0;
-    for(int i=0;i<sizeof(arr);i++){
+    for(int i=0;i<sizeof(arr)-1;i++){
         if(arr[i]=='('){
-            for(int j=i;j<sizeof(arr);j++){
+            for(int j=i+1;j<sizeof(arr);j++){
                 if(arr[j]==')'){
                     cnt++;
                 }

815c0709e815cf358e1abcd2ea8799d896e202ef
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-11-07 20:15:41 +0900

    Title: 최고의 13위치, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251107/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[25][25];
+
+int main() {
+    int N;
+    cin >> N;
+    
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int most=0;
+    for(int i=0;i<N;i++){
+        int sum=0;
+        for(int j=0;j<N-2;j++){
+            sum = arr[i][j]+arr[i][j+1]+arr[i][j+2];
+        }
+        most = max(most, sum);
+    }
+    cout << most;
+    // Please write your code here.
+    return 0;
+}

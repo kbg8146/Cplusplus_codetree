dda1763c02332c816c07c4dcca862be24173f6cf
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-11-08 14:07:02 +0900

    Title: 선두를 지켜라 3, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251108/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int a[1001];
+int b[1001];
+
+int atime, btime;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    int v=0,t=0,dist=0;
+    for(int i=0;i<N;i++){
+        cin >> v >> t;
+        for(int j=1;j<=t;j++){
+            dist += v;
+            a[atime++] = dist;
+        }
+        
+    }
+    dist=0;
+    for(int i=0;i<M;i++){
+        cin >> v >> t;
+        for(int j=1;j<=t;j++){
+            dist += v;
+            b[btime++] = dist;
+        }
+    }
+
+    int leader=0,cnt=0;
+    for(int i=0;i<max(atime,btime);i++){
+        if(a[i]>b[i]){
+            if(leader != 1){
+                cnt++;
+            }
+            leader =1;
+
+        }
+        else if(a[i]<b[i]){
+            if(leader != 2){
+                cnt++;
+            }
+            leader =2;
+        }
+        else{
+            if(leader != 0){
+                cnt++;
+            }
+            leader =0;
+            
+        }
+    }
+    cout << cnt;
+    // Please write your code here.
+    return 0;
+}

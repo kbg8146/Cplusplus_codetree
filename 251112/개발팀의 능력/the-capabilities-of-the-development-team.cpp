d65e5d615625b182894f5493a7ff5d45b0c4ea4b
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-11-12 17:08:05 +0900

    Title: 개발팀의 능력, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251112/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int arr[5];
+int sum, group1, group2, group3,tmp;
+
+int Diff(int i, int j, int k){
+    group1 = arr[i]+arr[j];
+    group2 = arr[k];
+    group3 = sum - group1 - group2;
+
+    tmp = min(group1, group2);
+    int least = min(tmp, group3);
+    tmp = max(group1,group2);
+    int most = max(tmp,group3);
+    return abs(most-least);
+}
+
+int main() {
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    int least = INT_MAX;
+    for(int i=0;i<5;i++){
+        for(int j=i+1;j<5;j++){
+            for(int k=j+1;k<5;k++){
+                least = min(least,Diff(i,j,k));
+            }
+        }
+    }
+    cout << least;
+    // Please write your code here.
+    return 0;
+}

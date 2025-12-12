d1d419c378f7bca0af35f10580f7c394fc973292
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-12 11:23:25 +0900

    Title: 야바위, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251212/야바위/ya-rock.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int a[100], b[100], c[100];
+int arr[4];
+
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i] >> b[i] >> c[i];
+    }
+    int point = 0;int most = 0;
+    for(int j=1;j<=3;j++){
+        arr[j] = 1;
+        for(int i=0;i<N;i++){
+            swap(arr[a[i]], arr[b[i]]);
+            if(arr[c[i]]){
+                point++;
+            }
+        }
+        arr[j] = 0; 
+        most = max(point,most);
+        point = 0;
+    }
+    cout << most;
+
+    // Please write your code here.
+
+    return 0;
+}

bcc73eac09bc2514aaae54cf25c2704a7aca4adf
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-20 16:03:11 +0900

    Title: 원소 값들의 최대 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251220/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
+++ 251220/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int n, m;
-int arr[101];
+int arr[100];
 
 int main() {
     cin >> n >> m;
-    
     for (int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
-    
-    int maxSum = 0;
-    
-    // 모든 시작 위치(1~N)에 대해 시도
-    for (int start = 1; start <= n; start++) {
-        int sum = 0;
-        int current = start;
-        
-        // 현재 시작 위치에서 M번 이동하며 합 계산
-        for (int step = 0; step < m; step++) {
-            sum += arr[current];        // 현재 위치의 값을 합계에 더함
-            current = arr[current];     // 현재 위치의 값이 가리키는 위치로 이동
+
+    int strat = 0, most = 0, sum = 0;
+    for(int j=1;j<=n;j++){
+        int pos = j;
+        for(int i=0;i<m;i++){
+            sum += arr[pos];
+            pos = arr[pos];
         }
-        
-        // 최댓값 갱신
-        maxSum = max(maxSum, sum);
+        most = max(sum,most);
+        sum = 0;
     }
-    
-    cout << maxSum << endl;
-    
+
+    cout << most;
+
+    // Please write your code here.
+
     return 0;
 }

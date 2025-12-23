9bf5ab1023874dbd0d35eba54f0459c8e627ba79
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-23 16:17:03 +0900

    Title: 최대 최소간의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251223/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n, k;
+int arr[100];
+
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sub=0; int least = INT_MAX;  
+    for(int j=1;j<=10000-k;j++){
+        for(int i=0;i<n;i++){
+
+            int min = j;
+            int max = j+k;
+
+            if(arr[i]<min){
+                sub += abs(arr[i]-min);
+            }
+            else if(arr[i]>max){
+                sub += abs(arr[i]-max);
+            }
+        }
+        least = min(least, sub);
+        sub=0;
+    }
+    cout << least;
+    // Please write your code here.
+
+    return 0;
+}

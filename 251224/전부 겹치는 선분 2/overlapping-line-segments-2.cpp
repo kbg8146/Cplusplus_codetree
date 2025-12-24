c9a27f0636758f1734e4cf41f8570c6869fb725d
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-24 17:48:13 +0900

    Title: 전부 겹치는 선분 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251224/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 251224/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <climits>
+#include <string>
 using namespace std;
 
 int n;
 int x1[100], x2[100];
-
+bool ans=0;
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
     }
+    int big=INT_MIN; int small = INT_MAX;
+    for(int skip=0;skip<n;skip++){
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(i==skip||j==skip) continue;
+                big = max(x1[i], x1[j]);
+                small = min(x2[i], x2[j]);
 
-    for (int i = 0; i < n; i++) { // i번째 선분을 제외해본다
-        int max_start = 1;   // 시작점 중 최댓값 (문제 조건상 최소 1)
-        int min_end = 100;   // 끝점 중 최솟값 (문제 조건상 최대 100)
+                if(big<small) {
+                    ans = 1;
+                }
+            }
 
-        for (int j = 0; j < n; j++) {
-            if (i == j) continue; // i번째는 제외
-
-            max_start = max(max_start, x1[j]);
-            min_end = min(min_end, x2[j]);
-        }
-
-        // 모든 j를 확인한 후, 공통 구간이 존재하는지 체크
-        if (max_start <= min_end) {
-            cout << "Yes";
-            return 0;
         }
     }
-cout << "No";
+    if(ans==1)
+        cout << "Yes";
+    else
+        cout << "No";
+    // Please write your code here.
+
+    return 0;
 }

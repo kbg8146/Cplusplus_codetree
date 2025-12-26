b8ae8ed6d9d471a4e74afc3548fa6b5d481f1791
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-26 20:11:51 +0900

    Title: 그래프 탐색, Time: 13ms, Memory: 0MB, Status: Passed - Codetree

--- 251226/그래프 탐색/graph-traversal.cpp
+++ 251226/그래프 탐색/graph-traversal.cpp
@@ -12,7 +12,7 @@ vector<int> graph[MAX_NUM+1];
 bool visited[MAX_NUM+1];
 
 void DFS(int vertex){
-    for(int i=0;i<(int) graph[vertex].size();i++){
+    for(int i=0;i<(int)graph[vertex].size();i++){
         int curr_v = graph[vertex][i];
         if(!visited[curr_v]){
             visited[curr_v] = true;
@@ -24,18 +24,17 @@ void DFS(int vertex){
 
 int main() {
     cin >> n >> m;
-
     int v1, v2;
     for (int i = 0; i < m; i++) {
         cin >> v1 >> v2;
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
     }
-
     visited[1] = true;
     DFS(1);
 
     cout << vertex_cnt;
+
     // Please write your code here.
 
     return 0;

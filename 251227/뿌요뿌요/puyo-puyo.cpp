28a650b18fef9509482e0770a86e7e6f1d330431
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-27 19:34:00 +0900

    Title: 뿌요뿌요, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251227/뿌요뿌요/puyo-puyo.cpp
+++ 251227/뿌요뿌요/puyo-puyo.cpp
@@ -6,8 +6,9 @@ int n;
 int grid[100][100];
 bool visited[100][100];
 int cnt=0;
-int max_cnt=1;
 int ans=1;
+int num_cnt[100];
+int max_cnt;
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
@@ -18,7 +19,7 @@ bool InRange(int x, int y){
 
 bool CanGo(int x, int y,int nx, int ny){
     if(!InRange(nx,ny)) return false;
-    if(visited[nx][ny]==1) return false;
+    if(visited[nx][ny]) return false;
     return true;
 }
 
@@ -28,11 +29,12 @@ int DFS(int x, int y){
         int nx = x + dx[i];
         int ny = y + dy[i];
         
-        if(grid[nx][ny]==grid[x][y]){
-            if(CanGo(x,y,nx,ny)){
+        
+        if(CanGo(x,y,nx,ny)){
+            if(grid[nx][ny]==grid[x][y]){
                 visited[nx][ny] = 1;
                 max_cnt++;
-                if(max_cnt==4) cnt++;
+                
                 DFS(nx,ny);
             } 
         }
@@ -48,21 +50,25 @@ int main() {
             cin >> grid[i][j];
         }
     }
-    visited[0][0] = 1;
-    //DFS(0,0);
-    for(int num=1;num<=100;num++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==num){
-                    ans = max(ans, DFS(i,j));
-                    max_cnt=0;
-                }
+    max_cnt=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(visited[i][j]==0){
+                visited[i][j]=1;
+                max_cnt=1;
+                int cnt1 = DFS(i,j);
+                if(cnt1>=4) cnt++;
+                num_cnt[(i)*n+j]= max(num_cnt[(i)*n+j],cnt1);
             }
         }
     }
+    int big=0;
+    for(int i=0;i<n*n;i++){
+        big = max(num_cnt[i],big);
+    }
 
 
-    cout << cnt <<" " <<ans;
+    cout << cnt <<" " <<big;
 
     return 0;
 }

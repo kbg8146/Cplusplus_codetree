56eb3d35a16535cc12a387194d1f99791058e126
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-30 18:45:32 +0900

    Title: 연속된 숫자 만들기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251230/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 251230/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -7,9 +7,9 @@ int main() {
     cin >> x[0] >> x[1] >> x[2];
     sort(x,x+3);
     a=x[0];b=x[1];c=x[2];
-
+    cout << a << " " << b << " " << c << endl;
     if(a+1==b && b+1==c) cout << 0;
-    else if((a+1 == b && b+2 == c)||(a+2 == b && b+1 == c)) cout << 1;
+    else if((a+1 == b && b+2 <= c)||(a+2 <= b && b+1 == c)) cout << 1;
     else cout << 2;
     // Please write your code here.
     return 0;

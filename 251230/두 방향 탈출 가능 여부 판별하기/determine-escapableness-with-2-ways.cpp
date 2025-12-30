060da6b1ef048c98360c150edf9e0c7091bb8fe9
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-30 13:56:33 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 251230/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 251230/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -1,47 +1,55 @@
 #include <iostream>
 
+#define MAX_NUM 100
+#define DIR_NUM 2
+
 using namespace std;
 
 int n, m;
-int grid[100][100];
-bool visited[100][100];
+int grid[MAX_NUM][MAX_NUM];
+bool visited[MAX_NUM][MAX_NUM];
 
-bool InRange(int x, int y){
-    return 0<=x && x<n && 0<=y && y<n;
+// 탐색하는 위치가 격자 범위 내에 있는지 여부를 반환합니다.
+bool InRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
 }
-bool CanGo(int x, int y){
-    if(!InRange(x,y)) return false;
-    if(visited[x][y]) return false;
-    if(grid[x][y]==0) return false;
+
+// 탐색하는 위치로 움직일 수 있는지 여부를 반환합니다.
+bool CanGo(int x, int y) {
+    if(!InRange(x, y))
+        return false;
+
+    if(visited[x][y] || grid[x][y] == 0)
+        return false;
+
     return true;
 }
-void DFS(int x, int y){
-    int dx[2]={0,1};
-    int dy[2]={1,0};
 
-    for(int i=0;i<2;i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(CanGo(nx,ny)){
-            visited[nx][ny] = 1;
-            DFS(nx,ny);
+void DFS(int x, int y) {
+    int dx[DIR_NUM] = {0, 1};
+    int dy[DIR_NUM] = {1, 0};
+
+    // 아래쪽과 오른쪽 각각에 대하여 DFS 탐색을 합니다.
+    for(int i = 0; i < DIR_NUM; i++) {
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if(CanGo(new_x, new_y)) {
+            visited[new_x][new_y] = 1;
+            DFS(new_x, new_y);
         }
     }
+    
 }
 
-int main() {
+int main() { 
     cin >> n >> m;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
             cin >> grid[i][j];
-        }
-    }
+
     visited[0][0] = 1;
-    DFS(0,0);
+    DFS(0, 0);
 
-    cout << visited[n-1][m-1];
-    // Please write your code here.
-
+    cout << visited[n - 1][m - 1];
     return 0;
 }

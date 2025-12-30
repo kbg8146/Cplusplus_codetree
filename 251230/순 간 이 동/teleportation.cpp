ffd24106421c1e3acef34ca892e0c96dc840d7af
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2025-12-30 17:42:59 +0900

    Title: 순 간 이 동, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251230/순 간 이 동/teleportation.cpp
+++ 251230/순 간 이 동/teleportation.cpp
@@ -6,7 +6,7 @@ int c1, c2 ,c3;
 int main() {
     cin >> A >> B >> x >> y;
     
-    c1 = B-A;
+    c1 = abs(B-A);
     c2 = abs(A-x) + abs(B-y);
     c3 = abs(A-y) + abs(B-x);
     //cout << c1 << " " << c2 << " " << c3 << endl;

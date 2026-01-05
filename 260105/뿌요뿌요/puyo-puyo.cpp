ba158a57d148ceb9656a9c4581bbc7f12c7fdf2a
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2026-01-05 10:36:28 +0900

    Title: 뿌요뿌요, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260105/뿌요뿌요/puyo-puyo.cpp
+++ 260105/뿌요뿌요/puyo-puyo.cpp
@@ -7,6 +7,7 @@ int n, color;
 int grid[100][100];
 bool visited[100][100];
 int cnt, bomb_cnt;
+int big;
 
 bool InRange(int x, int y){
     return 0<=x && x<n && 0<=y && y<n;
@@ -17,16 +18,16 @@ bool CanGo(int x, int y, int color){
     if(grid[x][y]!=color) return false;
     return true;
 }
-void DFS(int x, int y, int color){
+void DFS(int x, int y){
     int dx[4]={1,0,-1,0};
     int dy[4]={0,-1,0,1};
     for(int i=0;i<4;i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if(CanGo(nx,ny,color)){
-            cnt++;
+        if(CanGo(nx,ny,grid[x][y])){
             visited[nx][ny]=true;
-            DFS(nx,ny,color);
+            cnt++;
+            DFS(nx,ny);
         }
     }
 }
@@ -38,15 +39,20 @@ int main() {
             cin >> grid[i][j];
         }
     }
-    int big = 0;
-    for(int i=1;i<=100;i++){
-        DFS(0,0,i);
-        big = max(cnt,big);
-        if(cnt>=4){
-            bomb_cnt++;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(!visited[i][j] && grid[i][j]){
+                visited[i][j] = true;
+                cnt = 1;
+                DFS(i,j);
+
+                if(cnt>=4) bomb_cnt++;
+
+                big = max(big,cnt);
+            }
         }
-        cnt=0;
     }
+
     cout << bomb_cnt << " " << big;
     return 0;
 }

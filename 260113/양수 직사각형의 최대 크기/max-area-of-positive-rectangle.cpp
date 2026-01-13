3d4af283c85935911eb2714f8eb51910fab53598
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2026-01-13 13:38:17 +0900

    Title: 양수 직사각형의 최대 크기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260113/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 260113/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -14,39 +14,41 @@ int main() {
     }
     
     int count=0,ans=-1;
+    bool minus = true;
  
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
 
-            for(int x_offset = 0;x_offset<n;x_offset++){
-                for(int y_offset = 0;y_offset<m;y_offset++){
+            for(int x_offset = 0;x_offset<n-i;x_offset++){
+                for(int y_offset = 0;y_offset<m-j;y_offset++){
+                    minus = true;
                     count = 0;
                     int k,l;
                     for(k=i;k<=i+x_offset;k++){
                         for(l=j;l<=j+y_offset;l++){
                             if(grid[k][l]<0) {
                                 count=0;
+                                minus = false;
                                 break;
                             }
                             count++;
                             //cout << k << " , " << l << " : "<< grid[k][l] << endl;
                         }
-                        if(grid[k][l]<0) {
+                        if(!minus) {
                             count=0;
                             break;
                         }
                     }
-                    if(grid[k][l]<0) {
+                    if(!minus) {
                         count=0;
                         break;
                     }
-                    cout << count << endl;
-                    ans = max(ans,count);
+                    //cout << count << endl;
+                    if(minus) ans = max(ans,count);
                 }
             }
         }
     }
-
     cout << ans;
     // Please write your code here.
 

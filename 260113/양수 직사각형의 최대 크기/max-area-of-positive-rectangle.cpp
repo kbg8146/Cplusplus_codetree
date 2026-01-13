9ed8e8d7796e2e29b95e46c95c156091f79dc163
Bugeon Kim <109897476+kbg8146@users.noreply.github.com>
2026-01-13 13:51:41 +0900

    Title: 양수 직사각형의 최대 크기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260113/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 260113/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -2,57 +2,39 @@
 
 using namespace std;
 
-int n, m;
+int n, m, ans;
 int grid[20][20];
+
+bool PositiveRect(int x1, int y1, int x2, int y2){
+    for(int i=x1;i<=x2;i++){
+        for(int j=y1;j<=y2;j++){
+            if(grid[i][j] <= 0)
+                return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     cin >> n >> m;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> grid[i][j];
         }
     }
-    
-    int count=0,ans=-1;
-    bool minus = true;
- 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-
-            for(int x_offset = 0;x_offset<n-i;x_offset++){
-                for(int y_offset = 0;y_offset<m-j;y_offset++){
-                    
-                    minus = true;
-                    count = 0;
-                    
-                    int k,l;
-                    for(k=i;k<=i+x_offset;k++){
-                        for(l=j;l<=j+y_offset;l++){
-                            if(grid[k][l]<=0) {
-                                count=0;
-                                minus = false;
-                                break;
-                            }
-                            count++;
-                            //cout << k << " , " << l << " : "<< grid[k][l] << endl;
-                        }
-                        if(!minus) {
-                            count=0;
-                            break;
-                        }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            for(int k=i;k<n;k++){
+                for(int l=j;l<m;l++){
+                    if(PositiveRect(i,j,k,l)){
+                        ans = max(ans, (k-i+1)*(l-j+1));
                     }
-                    if(!minus) {
-                        count=0;
-                        break;
-                    }
-                    //cout << count << endl;
-                    if(minus) ans = max(ans,count);
                 }
             }
         }
     }
     cout << ans;
-    // Please write your code here.
 
     return 0;
 }
